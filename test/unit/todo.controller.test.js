92a78598bce84cce631652dded05f1e35461f3b3
Alexis Villegas Torres <alexisvt@gmail.com>
2020-04-05 16:43:16 -0600

    test(TodoController): modify the create test
    Modify the create test to test if it has been called using a todo item.

--- test/unit/todo.controller.test.js
+++ test/unit/todo.controller.test.js
@@ -1,9 +1,16 @@
 const TodoController = require('../../controllers/todo.controller');
 const TodoModel = require('../../model/todo.model');
+const httpMocks = require('node-mocks-http');
+const newTodo = require('../mock-data/new-todo.json');
 
 describe('TodoController.createTodo', () => {
+  let req, res, next;
+
   beforeEach(() => {
     TodoModel.create = jest.fn();
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
   });
 
   it('should have a createTodo funtion', () => {
@@ -11,7 +18,8 @@ describe('TodoController.createTodo', () => {
   });
 
   it('should call TodoModel.create', () => {
-    TodoController.createTodo();
-    expect(TodoModel.create).toBeCalled();
+    req.body = newTodo;
+    TodoController.createTodo(req, res, next);
+    expect(TodoModel.create).toBeCalledWith(newTodo);
   });
 });
