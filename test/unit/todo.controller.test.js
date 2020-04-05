7ebed1fe22a357d0c0ac4ec3766cb2430f541d50
Alexis Villegas Torres <alexisvt@gmail.com>
2020-04-05 16:21:25 -0600

    test(TodoController): verify create call

--- test/unit/todo.controller.test.js
+++ test/unit/todo.controller.test.js
@@ -1,7 +1,17 @@
 const TodoController = require('../../controllers/todo.controller');
+const TodoModel = require('../../model/todo.model');
 
 describe('TodoController.createTodo', () => {
+  beforeEach(() => {
+    TodoModel.create = jest.fn();
+  });
+
   it('should have a createTodo funtion', () => {
     expect(typeof TodoController.createTodo).toBe('function');
+  });
+
+  it('should call TodoModel.create', () => {
+    TodoController.createTodo();
+    expect(TodoModel.create).toBeCalled();
   });
 });
