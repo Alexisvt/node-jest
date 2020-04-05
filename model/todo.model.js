7081e99d54ad04497026150ec0f0bd2bd6554c6b
Alexis Villegas Torres <alexisvt@gmail.com>
2020-04-05 15:53:47 -0600

    feat(TodoModel): add TodoModel

---
+++ model/todo.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+  title: {
+    required: true,
+    type: String,
+  },
+  done: {
+    required: true,
+    type: String,
+  },
+});
+
+const TodoModel = mongoose.model('Todo', TodoSchema);
+
+module.exports = TodoModel;
